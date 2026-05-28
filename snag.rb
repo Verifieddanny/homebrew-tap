adbb4a607217586f7d264d1dabeb47599d4a6e96
Daniel Nwachukwu <dannyclassic56@gmail.com>
2026-05-28 01:10:36 +0100

    Update snag.rb

--- snag.rb
+++ snag.rb
@@ -2,7 +2,7 @@ class Snag < Formula
   desc "Grab media from anywhere. Paste a link. Get your video."
   homepage "https://github.com/Verifieddanny/snag"
   url "https://github.com/Verifieddanny/snag/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "9c1bf5c6e44aa2eb619161c552a04772cbb4bb29e1248064e22918f7bdb2001c"
   license "MIT"
 
   depends_on "go" => :build
